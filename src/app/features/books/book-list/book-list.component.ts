fbfd1fc2129e2eab29f2e0893cc5d1dfd2209db5
Thomas Brunet <thomas.brunet@fuzeo.fr>
2025-03-31 13:01:32 +0200

    add pagination

--- src/app/features/books/book-list/book-list.component.ts
+++ src/app/features/books/book-list/book-list.component.ts
@@ -4,15 +4,16 @@ import {
   computed,
   inject,
 } from '@angular/core';
-import { CurrencyPipe, DecimalPipe } from '@angular/common';
+import {AsyncPipe, CurrencyPipe, DecimalPipe} from '@angular/common';
 import { RowComponent } from './row/row.component';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { RouterLink } from '@angular/router';
+import {ActivatedRoute, Router, RouterLink} from '@angular/router';
 import { BookService } from '../../../shared/services/book.service';
+import {debounceTime, switchMap} from 'rxjs';
 
 @Component({
   selector: 'app-book-list',
-  imports: [CurrencyPipe, DecimalPipe, RowComponent, RouterLink],
+  imports: [CurrencyPipe, DecimalPipe, RowComponent, RouterLink, AsyncPipe],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -20,7 +21,14 @@ import { BookService } from '../../../shared/services/book.service';
 })
 export class BookListComponent {
   readonly #bookService = inject(BookService);
-  readonly books = toSignal(this.#bookService.getBooks());
+  readonly #route = inject(ActivatedRoute)
+  readonly #router = inject(Router)
+  readonly books$ = this.#route.queryParams.pipe(
+    debounceTime(300),
+    switchMap(({ limit, skip }) => this.#bookService.getBooks(limit || 10, skip ||0))
+  )
+
+  books = toSignal(this.books$);
 
   readonly stats = computed(() => {
     const books = this.books();
@@ -33,4 +41,17 @@ export class BookListComponent {
     const avgPrice = totalPrice / books.length;
     return { totalQuantity, totalPrice, avgPrice };
   });
+
+  nextPage(){
+    const limit = parseInt(this.#route.snapshot.queryParams['limit']) || 10;
+    const skip = parseInt(this.#route.snapshot.queryParams['skip'] || 0) + limit;
+    this.#router.navigate([], {relativeTo: this.#route, queryParamsHandling: 'merge', queryParams: {skip, limit}});
+  }
+
+  previousPage(){
+    const limit = this.#route.snapshot.queryParams['limit'] || 10;
+    let skip = (this.#route.snapshot.queryParams['skip'] || 0) - limit;
+    skip = skip < 0 ? 0 : skip;
+    this.#router.navigate([], {relativeTo: this.#route, queryParamsHandling: 'merge', queryParams: {skip, limit}});
+  }
 }
--- src/app/shared/services/book.service.ts
+++ src/app/shared/services/book.service.ts
@@ -1,25 +1,28 @@
-import { inject, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Book } from '../interfaces/book.interface';
-import { delay } from 'rxjs';
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Book} from '../interfaces/book.interface';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({providedIn: 'root'})
 export class BookService {
   readonly #http = inject(HttpClient);
 
-  getBooks() {
-    return this.#http.get<Book[]>('/api/books').pipe(delay(1000));
+  getBooks(limit = 10, skip = 0) {
+    return this.#http.get<Book[]>('/api/books', {
+      params: {
+        skip, limit
+      }
+    });
   }
 
   getBook(id: number) {
-    return this.#http.get<Book>('/api/books/' + id).pipe(delay(1000));
+    return this.#http.get<Book>('/api/books/' + id);
   }
 
   createBook(value: Omit<Book, 'id'>) {
-    return this.#http.post<Book>('/api/books', value).pipe(delay(1000));
+    return this.#http.post<Book>('/api/books', value);
   }
 
   updateBook(id: number, value: Omit<Book, 'id'>) {
-    return this.#http.put<Book>('/api/books/' + id, value).pipe(delay(1000));
+    return this.#http.put<Book>('/api/books/' + id, value);
   }
 }
